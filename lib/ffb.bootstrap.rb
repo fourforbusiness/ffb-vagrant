1b591fc4392062cd2b95854c698a0feed11ab2be
Tobias Fischer <tobias.fischer@4fb.de>
2018-04-23 14:48:28 +0200

    changed urls

--- lib/ffb.bootstrap.rb
+++ lib/ffb.bootstrap.rb
@@ -1,9 +1,8 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 #
-# 
+#
 # This file is the entrypoint that loads more ruby-code to execute the vagrant configuration
-
 current_working_dir = File.dirname(File.expand_path(__FILE__))
 vagrant_ffb_dirname = ".ffb"
 vagrant_ffb_path = "#{current_working_dir}/#{vagrant_ffb_dirname}"
@@ -13,11 +12,10 @@ require 'optparse'
 ffb_tools_filename = "ffb.tools.rb"
 ffb_tools_path = "#{current_working_dir}/#{ffb_tools_filename}"
 if File.file?(ffb_tools_path)
-  require ffb_tools_path
+  require_relative ffb_tools_path
 end
 
 vagrant_ffb_path = "#{current_working_dir}"
-vagrant_environment_separator = "_"
 vagrant_main_script_filename = "ffb.vagrant.rb"
 vagrant_environment = "prod"
 
@@ -46,7 +44,7 @@ $*.each do |arg|
   end
 end
 
-remote_gist_base_uri = "https://gist.githubusercontent.com/tfr-ffb/7e1ac6dcdc7d168e63ecb87601a75821/raw/"
+remote_git_base_uri = "https://raw.githubusercontent.com/fourforbusiness/ffb-vagrant"
 filename_mapping = [
     { remote: vagrant_main_script_filename, local: vagrant_main_script_filename },
     { remote: "ansible.cfg", local: "ansible.cfg" },
@@ -65,16 +63,16 @@ unless vagrant_environment == VAGRANT_ENVIRONMENT::LOCAL
 #download the correct files from gist
   filename_mapping.each do |filename|
     # build local and remote uri
-    remote_uri = "#{remote_gist_base_uri}#{filename[:remote]}#{vagrant_environment_separator}#{vagrant_environment}"
+    remote_uri = "#{remote_git_base_uri}/#{vagrant_environment}/lib/#{filename[:remote]}"
     local_uri = "#{vagrant_ffb_path}/#{filename[:local]}"
     begin
       file_contents_downloaded = Net::HTTP.get(URI.parse(remote_uri))
       # if remote files cant be found, use production environment files
       # this helps for diving environmental settings in file or configuration related environments
       # for example the build environment uses the same files like the production environment
-      # but the build environment will not ask for host-manager settings
-      if(file_contents_downloaded.match(/^404: Not Found/))
-        remote_uri = "#{remote_gist_base_uri}#{filename[:remote]}#{vagrant_environment_separator}#{VAGRANT_ENVIRONMENT::PRODUCTION}"
+      # but the build environment will not ask for host-manager settings, the difference is configuration based
+      if(file_contents_downloaded.match(/^Not Found/))
+        remote_uri = "#{remote_git_base_uri}/#{VAGRANT_ENVIRONMENT::PRODUCTION}/lib/#{filename[:remote]}"
         file_contents_downloaded = Net::HTTP.get(URI.parse(remote_uri))
       end
       File.write(local_uri, file_contents_downloaded)
